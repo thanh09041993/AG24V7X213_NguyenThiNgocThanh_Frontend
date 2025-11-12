06661d79e11af325ebc63a3ed722932f0cd5d6b8
thanh09041993 <you@example.com>
2025-11-12 20:55:35 +0700

    Cai dat trang loi 404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page">
+    <p>
+      Oops, không thể tìm thấy trang. Trở về
+      <router-link to="/">trang chủ.</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style scoped>
+.page {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 18px;
+}
+</style>
